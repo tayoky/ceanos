d8d68e309e9ab92e9f125157daaf716b19ab101d
asdasda3456 <contact.minceraftyt@gmail.com>
2024-11-06 14:51:45 +0200

    preparing to merge a pr

--- src/cpuinfo.h
+++ src/cpuinfo.h
@@ -3,9 +3,12 @@
 #include <stdint.h>
 
 typedef struct {
-    char vendor[30];
+    char vendor[13];
     uint32_t model;
     uint32_t family;
+    uint32_t stepping;
+    uint32_t extended_model;
+    uint32_t extended_family;
 } CPUinfo;
 
 void get_cpu_info(CPUinfo* cpu_info); 
