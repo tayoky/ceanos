5dc9be8135f93174e56c0f9343a79d667ad3fc77
tayoky <stanislas343@gmail.com>
2024-11-15 18:51:29 +0100

    add function delcaration for vfs

--- src/fs/vfs.h
+++ src/fs/vfs.h
@@ -10,31 +10,20 @@
 #include <types.h>
 
 
-typedef struct {
-        char name[MAX_FILE_NAME];
-        char data[MAX_FILE_SIZE];
-        unsigned int size;
-} File;
-
-typedef struct {
-        File files[MAX_FILES];
-        int file_count;
-} VFS;
-
 struct vfs_node_struct ;
 typedef inode;
 
 //functions
-typedef ssize_t (*read_type_t) (struct fs_node *,  off_t, size_t, uint8_t *);
-typedef ssize_t (*write_type_t) (struct fs_node *, off_t, size_t, uint8_t *);
-typedef void (*open_type_t) (struct fs_node *, unsigned int flags);
-typedef void (*close_type_t) (struct fs_node *);
-typedef int (*create_type_t) (struct fs_node *, char *name, mode_t permission);
-typedef int (*unlink_type_t) (struct fs_node *, char *name);
-typedef int (*mkdir_type_t) (struct fs_node *, char *name, mode_t permission);
-typedef int (*chmod_type_t) (struct fs_node *, mode_t mode);
-typedef int (*chown_type_t) (struct fs_node *, uid_t, gid_t);
-typedef int (*set_size_type_t) (struct fs_node *,size_t);
+typedef ssize_t (*read_type_t) (vfs_node *,  off_t, size_t, uint8_t *);
+typedef ssize_t (*write_type_t) (vfs_node *, off_t, size_t, uint8_t *);
+typedef void (*open_type_t) (vfs_node *);
+typedef void (*close_type_t) (vfs_node *);
+typedef int (*create_type_t) (vfs_node *, char *name, mode_t permission);
+typedef int (*unlink_type_t) (vfs_node *, char *name);
+typedef int (*mkdir_type_t) (vfs_node *, char *name, mode_t permission);
+typedef int (*chmod_type_t) (vfs_node *, mode_t mode);
+typedef int (*chown_type_t) (vfs_node *, uid_t, gid_t);
+typedef int (*set_size_type_t) (vfs_node *,size_t);
 
 typedef struct vfs_node_struct {
         char name[256];
@@ -60,3 +49,18 @@ typedef struct vfs_node_struct {
         chown_type_t chown;
         chmod_type_t chmod;
 } vfs_node ;
+
+extern vfs_node *vfs_root_node;
+
+int vfs_int();
+
+ssize_t vfs_read(vfs_node *node,off_t offset,size_t count,void *buffer);
+ssize_t vfs_write(vfs_node *node,off_t offset,size_t count,void *buffer);
+int vfs_open(vfs_node *node);
+int vfs_close(vfs_node *node);
+int vfs_create(vfs_node *node,char *name,mode_t permission);
+int vfs_mkdir(vfs_node *node,char *name,mode_t permission);
+int vfs_unlink(vfs_node *node,char *name);
+int vfs_set_size(vfs_node *node,size_t new_size);
+int vfs_chown(vfs_node *node,uid_t user,gid_t group);
+int vfs_chmod(vfs_node *node,mode_t permission);
