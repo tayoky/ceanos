f9c7065d4cf1fc0ea8e6823247fba75c0e39021a
tayoky <stanislas343@gmail.com>
2024-11-16 19:03:42 +0100

    new tmpfs

--- src/fs/tmpfs.c
+++ src/fs/tmpfs.c
@@ -9,6 +9,9 @@ typedef struct inode_struct{
     int type;
 }inode;
 
+#define TMPFS_TYPE_DIRECTORY 1
+#define TMPFS_TYPE_FILE 2
+
 #define VFS_DRIVER
 #include "tmpfs.h"
 #include <fs/vfs.h>
@@ -18,24 +21,33 @@ int init_tmpfs(){
     vfs_node *root;
     root = tmpfs_create_dir(NULL);
     //mount as root
+    vfs_mount("/",root);
 }
 vfs_node *tmpfs_create_dir(inode *parent){
+    vfs_node *node=tmpfs_create_file(parent);
+
+    node->mkdir = tmpfs_mkdir;
+    node->type = VFS_NODE_TYPE_FOLDER;
+    node->inode->type = TMPFS_TYPE_DIRECTORY;
+    return node;
+}
+
+vfs_node *tmpfs_create_file(inode *parent){
     vfs_node *node=kmalloc(sizeof(vfs_node));
     node->inode = kmalloc(sizeof(inode));
     node->permmision = 0777;
     
+    node->inode->type = TMPFS_TYPE_FILE;
     node->inode->parent = parent;
     node->inode->child = NULL;
     node->inode->brother = NULL;
+    node->inode->size = 0;
+    node->size = 0;
     if(parent){
         node->inode->brother = parent->child;
         parent->child = node->inode;
     }
-
-    node->mkdir = tmpfs_mkdir;
-    return node;
 }
-
 
 int tmpfs_mkdir(vfs_node *node,char *name){
     vfs_node *dir =tmpfs_create_dir(node->inode);
@@ -51,4 +63,21 @@ int tmpfs_close(vfs_node *node){
     kfree(node->inode);
 }
 
+struct dirrent tmpsfs_readdir(vfs_node *node,uint32_t index){
+    //first the . path
+    if(index == 0){
+        struct dirrent *ret = kmalloc(sizeof(struct dirrent));
+        strcpy(ret->name,VFS_SPECIAL_PATH_SELF);
+    }
 
+    //first the .. path
+    if(index == 1){
+        struct dirrent *ret = kmalloc(sizeof(struct dirrent));
+        strcpy(ret->name,VFS_SPECIAL_PATH_PARENT);
+    }
+
+    index -= 2;
+    for(uint32_t i=0;i<index;i++){
+        
+    }
+}
--- src/fs/tmpfs.h
+++ src/fs/tmpfs.h
@@ -3,4 +3,5 @@
 
 int init_tmpfs();
 vfs_node *tmpfs_create_dir(inode *parent);
+vfs_node *tmpfs_create_file(inode *parent);
 int tmpfs_mkdir(vfs_node *node,char *name);
